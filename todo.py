5c7f5216c05af784ae46d172464d70d6c4b9ddc0
iiii-uwuu <2291937845@qq.com>
2026-05-13 14:03:00 +0800

    fix:语法错误

--- todo.py
+++ todo.py
@@ -1,12 +1,12 @@
 import json
 import os
 
-TASKS_FILE="tasks,json"
+TASKS_FILE="tasks.json"
 
 def load_tasks():
     if not os.path.exists(TASKS_FILE):
         return []
-    with open(TASKS_FILE,"r",encodin="utf-8")as f:
+    with open(TASKS_FILE,"r",encoding="utf-8")as f:
         return json.load(f)
 
 def save_tasks(tasks):
